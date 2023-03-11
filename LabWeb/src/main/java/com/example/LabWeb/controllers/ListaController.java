6321e50e429f117ee3c6d770759f216a1f474835
Freddy <ferreirafreddy39@gmail.com>
2023-03-11 16:09:36 -0300

    arreglo de cors

--- LabWeb/src/main/java/com/example/LabWeb/controllers/ListaController.java
+++ LabWeb/src/main/java/com/example/LabWeb/controllers/ListaController.java
@@ -3,6 +3,7 @@ package com.example.LabWeb.controllers;
 import com.example.LabWeb.models.AnalisisModel;
 import com.example.LabWeb.services.ListaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,13 +12,10 @@ import java.util.*;
 
 @RestController
 @RequestMapping("/api")
-
 public class ListaController {
 
     @Autowired
     ListaService listaService;
-
-    AnalisisModel analisisModel;
 
     @GetMapping("/analisis")
     public List<AnalisisModel> getAnalisis() {
